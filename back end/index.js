c94c15864160472884a1187da21181fddec7326d
krishnangaya3 <krishnangayathri46@gmail.com>
2022-03-08 00:12:04 +0530

    backend userslist

--- back end/index.js
+++ back end/index.js
@@ -14,25 +14,32 @@ app.use(express.json());
 app.use("/login", loginRouter);
 app.use("/signup", signupRouter);
 
-app.post("/contactinfo", function (req, res) {
-  var resume = {
-    phone: req.body.phone,
-    email: req.body.email,
-    website: req.body.website,
-    address: req.body.address,
-    experience: req.body.experience,
-  };
-  console.log(resume);
-  const resumeDetails = new ResumeInfo(resume);
-  resumeDetails.save(function (err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(result);
-      res.json(result);
-    }
+app.get("/userslist", function (req, res) {
+  UserInfo.find().then(function (users) {
+    console.log("users::", users);
+    res.json(users);
   });
 });
+
+// app.post("/contactinfo", function (req, res) {
+//   var resume = {
+//     phone: req.body.phone,
+//     email: req.body.email,
+//     website: req.body.website,
+//     address: req.body.address,
+//     experience: req.body.experience,
+//   };
+//   console.log(resume);
+//   const resumeDetails = new ResumeInfo(resume);
+//   resumeDetails.save(function (err, result) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(result);
+//       res.json(result);
+//     }
+//   });
+// });
 
 // Port number
 app.listen(5000, () => {
