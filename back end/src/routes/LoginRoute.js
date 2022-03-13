a0c0fb295185594082ac656afdf4bf097c3a6673
krishnangaya3 <krishnangayathri46@gmail.com>
2022-03-13 16:28:11 +0530

    Fixing the redirection issue for bad signin

--- back end/src/routes/LoginRoute.js
+++ back end/src/routes/LoginRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const loginRouter = express.Router();
 const UserInfo = require("../models/UsersDB");
-const JWT_SECRET="This is your secret key"
+const JWT_SECRET = "This is your secret key";
 loginRouter.get("/", function (req, res) {
   res.render("login", {});
 });
@@ -17,11 +17,18 @@ loginRouter.post("/verifyuser", function (req, res) {
   UserInfo.findOne(
     { username: username, password: password },
     function (err, user) {
-      console.log(user);
-      const token = jwt.sign({ id:user._id }, JWT_SECRET, {
-        expiresIn: 3600
-      })
-      res.json({user:user,token:token});
+      console.log("retrieved user::", user);
+      if (err) {
+        console.log(err);
+      }
+      if (user) {
+        const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+          expiresIn: 3600,
+        });
+        res.json({ user: user, token: token });
+      } else {
+        console.log("Username or Password mismatch!");
+      }
     }
   );
 });
