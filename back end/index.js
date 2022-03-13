1d70d72e48e77d15818119fd0996b35162446ad9
Ashiquehaneefa <93711110+Ashiquehaneefa@users.noreply.github.com>
2022-03-13 16:41:52 +0530

    Update index.js

--- back end/index.js
+++ back end/index.js
@@ -7,6 +7,8 @@ const loginRouter = require("./src/routes/LoginRoute");
 const signupRouter = require("./src/routes/SignupRoute");
 const emailvalidator = require("email-validator");
 
+const port = process.env.PORT || 5000
+
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -104,6 +106,6 @@ app.post("/resumedetails", function (req, res) {
 });
 
 // Port number
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Listening on port 5000");
 });
